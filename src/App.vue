896e14078a8b19099f9f9e04077f9dbbba2c3d3d
MIGUELGM98 <gonzalez.martinez.16040@itsmante.edu.mx>
2022-01-28 10:52:42 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mx-auto max-w-7xl px-5 py-20">
+    <p>Miguel</p>
+    <div id="panzoom-element">
+      <img src="https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/virtual_console_nintendo_3ds_7/SI_3DSVC_SuperMarioBros_image1600w.jpg" alt="">
+    </div>
+    <button id="zoom" class="border">zoom In</button>
+    <button id="zoomOut" class="ml-5 border">zoom out</button>
+  </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Panzoom from '@panzoom/panzoom'
 
-export default {
-  name: 'App',
+export default ({
   components: {
-    HelloWorld
-  }
-}
+    
+  },
+  mounted(){
+    const elem = document.getElementById('panzoom-element')
+    const button = document.getElementById('zoom')
+    const button2 = document.getElementById('zoomOut')
+    const panzoom = Panzoom(elem, {
+      maxScale: 5,
+    })
+    panzoom.zoom(2, { animate: true })
+    button.addEventListener('click', panzoom.zoomIn)
+    button2.addEventListener('click', panzoom.zoomOut)
+    elem.parentElement.addEventListener('wheel', panzoom.zoomWithWheel)
+  },
+  setup() {
+  },
+})
 </script>
+<style lang="scss" scoped>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
